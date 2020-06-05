389fbfd933df0d0cfb2c729599fd683b6e4a1597
Nightlan <437798348@qq.com>
2020-06-05 11:08:51 +0800

    更新算法

---
+++ productExceptSelf/main.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+/*
+给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+
+
+
+示例:
+
+输入: [1,2,3,4]
+输出: [24,12,8,6]
+
+
+提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+
+说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+
+进阶：
+你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/product-of-array-except-self
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func productExceptSelf1(nums []int) []int {
+	preSum := make([]int, len(nums))
+	sufSum := make([]int, len(nums))
+	sum := 1
+	for i := 0; i < len(nums); i++ {
+		preSum[i] = sum
+		sum *= nums[i]
+	}
+	sum = 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		sufSum[i] = sum
+		sum *= nums[i]
+	}
+	res := make([]int, len(nums))
+	for i := range nums {
+		res[i] = preSum[i] * sufSum[i]
+	}
+	return res
+}
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	sum := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = sum
+		sum *= nums[i]
+	}
+	sum = 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] = sum * res[i]
+		sum *= nums[i]
+	}
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(productExceptSelf(nums))
+}
